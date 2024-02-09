9d34ad51d938bad333449684f7a6535c19f1d596
永岡優 <nagaoka@nagaoka-yuutainoMacBook-Pro.local>
2024-02-09 22:26:33 +0900

    Add w14

--- W14/w14_HeatMap.js
+++ W14/w14_HeatMap.js
@@ -105,13 +105,13 @@ function createHeatmap(prec1, prec2) {
 
 document.getElementById('idUpButton').addEventListener('click', function() {
     selectedCell = Math.max(0, selectedCell - 1);  // Ensure selectedCell doesn't go below 0
-    drawHeatmap();  // Assuming this is the function that draws the heatmap
+    creaHeatmap(prec1, prec2);  // Assuming this is the function that draws the heatmap
 });
 
 document.getElementById('idDownButton').addEventListener('click', function() {
     selectedCell = selectedCell + 1;  // Increment selectedCell
     // You might want to add logic to ensure selectedCell doesn't exceed the number of cells
-    drawHeatMap();  // Redraw the heatmap
+    createHeatMap(prec1, prec2);  // Redraw the heatmap
 });
 
 // Example usage
