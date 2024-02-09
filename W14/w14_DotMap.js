93f11b39e2d60872f155759b2692b714449aa67c
永岡優 <nagaoka@nagaoka-yuutainoMacBook-Pro.local>
2024-02-10 03:04:24 +0900

    Add w14

--- W14/w14_DotMap.js
+++ W14/w14_DotMap.js
@@ -81,8 +81,9 @@ function drawDotMap(data) {
       .style("left", xPosition + "px")
       .style("top", yPosition + "px")
       .select("#value")
-      .text(function(d,i){
-        i + "(" +d[0] + "," + d[1]+")"});
+      .text(`${function(d,i){
+        return i;
+      }}, (${d[0]}, ${d[1]})`);
 
     d3.select("#tooltip").classed("hidden", false);
   });
